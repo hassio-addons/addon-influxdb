223d1e9fe840a0e1760b1e0a4d6e4ec48413adef
Franck Nijhof <frenck@geekchimp.com>
2018-05-27 17:09:10 +0200

    :ambulances: Fixes SSL handling for Chronograf & Kapacitor

--- influxdb/rootfs/etc/cont-init.d/30-kapacitor.sh
+++ influxdb/rootfs/etc/cont-init.d/30-kapacitor.sh
@@ -6,5 +6,9 @@
 # shellcheck disable=SC1091
 source /usr/lib/hassio-addons/base.sh
 
+if hass.config.get 'ssl'; then
+    sed -i "s/http:/https:/" /etc/kapacitor/kapacitor.conf
+fi
+
 sed -i "s/password.*/password = \"${HASSIO_TOKEN}\"/" \
     /etc/kapacitor/kapacitor.conf
