ebc77ae685fe6ad8337fc3b759d6d05b1c2cc7ce
Franck Nijhof <frenck@addons.community>
2018-08-15 22:39:27 +0200

    :ambulance: Stop NGinx from throwing log errors

--- influxdb/rootfs/etc/cont-init.d/11-nginx.sh
+++ influxdb/rootfs/etc/cont-init.d/11-nginx.sh
@@ -9,6 +9,8 @@ source /usr/lib/hassio-addons/base.sh
 declare certfile
 declare keyfile
 
+mkdir -p /var/log/nginx
+
 if hass.config.true 'ssl'; then
     rm /etc/nginx/nginx.conf
     mv /etc/nginx/nginx-ssl.conf /etc/nginx/nginx.conf
