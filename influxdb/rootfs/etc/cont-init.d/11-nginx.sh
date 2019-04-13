2ae79f3d1a5db19a19a7d89d3b9aa683de811b13
Franck Nijhof <frenck@addons.community>
2019-04-13 19:20:47 +0200

    :sparkles: Adds Hassio Ingress support

--- influxdb/rootfs/etc/cont-init.d/11-nginx.sh
+++ influxdb/rootfs/etc/cont-init.d/11-nginx.sh
@@ -3,18 +3,36 @@
 # Community Hass.io Add-ons: InfluxDB
 # Configures NGINX for use with the Chronograf
 # ==============================================================================
+declare admin_port
 declare certfile
+declare ingress_interface
+declare ingress_port
+declare ingress_entry
 declare keyfile
 
-mkdir -p /var/log/nginx
+admin_port=$(bashio::addon.port 80)
+ingress_entry=$(bashio::addon.ingress_entry)
+if bashio::var.has_value "${admin_port}"; then
+    bashio::config.require.ssl
 
-if bashio::config.true 'ssl'; then
-    rm /etc/nginx/nginx.conf
-    mv /etc/nginx/nginx-ssl.conf /etc/nginx/nginx.conf
+    if bashio::config.true 'ssl'; then
+        certfile=$(bashio::config 'certfile')
+        keyfile=$(bashio::config 'keyfile')
 
-    certfile=$(bashio::config 'certfile')
-    keyfile=$(bashio::config 'keyfile')
+        mv /etc/nginx/servers/direct-ssl.disabled /etc/nginx/servers/direct.conf
+        sed -i "s/%%certfile%%/${certfile}/g" /etc/nginx/servers/direct.conf
+        sed -i "s/%%keyfile%%/${keyfile}/g" /etc/nginx/servers/direct.conf
 
-    sed -i "s/%%certfile%%/${certfile}/g" /etc/nginx/nginx.conf
-    sed -i "s/%%keyfile%%/${keyfile}/g" /etc/nginx/nginx.conf
+    else
+        mv /etc/nginx/servers/direct.disabled /etc/nginx/servers/direct.conf
+    fi
+
+    sed -i "s/%%port%%/${admin_port}/g" /etc/nginx/servers/direct.conf
+    sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/servers/direct.conf
 fi
+
+ingress_port=$(bashio::addon.ingress_port)
+ingress_interface=$(bashio::addon.ip_address)
+sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
+sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
+sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/servers/ingress.conf
