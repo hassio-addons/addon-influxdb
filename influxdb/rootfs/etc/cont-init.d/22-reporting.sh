f4f0ad42d93c6674535e44632726c18e83e72aa6
Franck Nijhof <frenck@addons.community>
2019-04-13 19:26:01 +0200

    :hammer: Simplifies add-on code logic

--- influxdb/rootfs/etc/cont-init.d/22-reporting.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/with-contenv bashio
-# ==============================================================================
-# Community Hass.io Add-ons: InfluxDB
-# Configures usage reporting to InfluxDB
-# ==============================================================================
-if bashio::config.false 'reporting'; then
-    sed -i 's/reporting-disabled=.*/reporting-disabled=true/' /etc/influxdb/influxdb.conf
-    bashio::log.info "Reporting of usage stats to InfluxData is disabled."
-fi
--- influxdb/rootfs/etc/cont-init.d/influxdb.sh
+++ influxdb/rootfs/etc/cont-init.d/influxdb.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/with-contenv bashio
 # ==============================================================================
 # Community Hass.io Add-ons: InfluxDB
-# Configures authentication
+# Configures InfluxDB
 # ==============================================================================
+
+# Configures authentication
 if bashio::config.true 'auth'; then
     sed -i 's/auth-enabled=.*/auth-enabled=true/' /etc/influxdb/influxdb.conf
 else
     bashio::log.warning "InfluxDB authentication protection is disabled!"
     bashio::log.warning "This is NOT recommended!!!"
+fi
+
+# Configures usage reporting to InfluxDB
+if bashio::config.false 'reporting'; then
+    sed -i 's/reporting-disabled=.*/reporting-disabled=true/' /etc/influxdb/influxdb.conf
+    bashio::log.info "Reporting of usage stats to InfluxData is disabled."
 fi
